9f76f24e9915f4917c6fa40a8c5d153c899c0151
Justin Rainbow <justin.rainbow@gmail.com>
2011-11-02 13:50:40 -0700

    Adding checks to make sure only one attack can take place at a time

--- scripts/retaliation.js
+++ scripts/retaliation.js
@@ -1,9 +1,21 @@
 module.exports = function(robot) {
-    var exec = require('child_process').exec;
+    var exec = require('child_process').exec,
+        lock = false;
 
     robot.hear(/shoot ([a-z]+)/i, function (msg) {
+        if (true === lock) {
+            return msg.send("Settle down there tiger.");
+        }
         
-        exec("python /Users/sheknowsdev/Retaliation/retaliation.py " + msg[1]);
+        lock = true;
         
+        exec(
+            "python /Users/sheknowsdev/Retaliation/retaliation.py " + msg.match[1],
+            function (error, stdout, stderr) {
+                lock = false;
+            }
+        );
+        
+        return msg.send("Aye aye, Captain!");
     });
 }
