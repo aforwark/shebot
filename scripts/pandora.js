0982776093ef0c081d6de4d17c6bb2286cd59f48
Justin Rainbow <justin.rainbow@gmail.com>
2011-11-03 16:09:04 -0700

    Adding the option to skip

--- scripts/pandora.js
+++ scripts/pandora.js
@@ -3,8 +3,12 @@ module.exports = function(robot) {
     var fs = require('fs'),
         exec = require('child_process').exec;
     
+    function cmd(command, cb) {
+        exec('echo '+command+' > ' + process.env.HUBOT_PANDORA_CTL, cb);
+    }
+    
     robot.hear(/pandora play/, function (msg) {
-        exec('echo p > ' + process.env.HUBOT_PANDORA_CTL, function (err, stdout, stderr) {
+        cmd('p', function (err, stdout, stderr) {
             if (err) {
                 msg.send('Error communicating with pianobar!');
                 return;
@@ -12,5 +16,16 @@ module.exports = function(robot) {
             
             msg.send('Now playing... or maybe paused...');
         })
+    });
+    
+    robot.hear(/^pandora next/, function (msg) {
+        cmd('n', function (err, stdout, stderr) {
+            if (err) {
+                msg.send('Error communicating with pianobar!');
+                return;
+            }
+            
+            msg.send('Moving on...');
+        });
     });
 };
