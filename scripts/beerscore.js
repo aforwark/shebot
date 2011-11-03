b982881002f1df6999ca09578dc83a7a47a55a8c
Justin Rainbow <justin.rainbow@gmail.com>
2011-11-03 10:58:06 -0700

    beerscore tweaks

--- scripts/beerscore.js
+++ scripts/beerscore.js
@@ -1,55 +1,52 @@
 (function() {
     var sprintf;
-    
+
     sprintf = require("sprintf").sprintf;
-    
+
     module.exports = function(robot) {
         var baseUrl = 'http://caedmon.net/beerscore/';
 
-        return robot.respond(/beer ?score\s+(.+?)/i, function(msg) {
+        return robot.hear(/^beer ?score\s+(.+)/i, function(msg) {
             var beer;
             beer = escape(msg.match[1]);
+            console.log("Request: " + baseUrl+beer);
             return msg.http(baseUrl+beer).get()(function(err, res, body) {
                 var object, found = 0;
                 if (res.statusCode === 404) {
                     return msg.send('Beer not found.');
                 } else {
                     object = JSON.parse(body);
-                    
+
                     switch (object.type) {
                         case 'REFINE':
-                            msg.send(sprintf('There were %d beers found. %s', object.num, object.searchurl));
-                            break;
-                        
+                            return msg.send(sprintf('There were %d beers found. %s', object.num, object.searchurl));
+
                         case 'SEARCH':
                             for (var i = 0, len = object.beer.length; i < len; i++) {
                                 var beer = object.beer[i];
-                                
+
                                 if (beer.score) {
                                     msg.send(sprintf('rating for "%s" = %s (%s)', beer.name, beer.score, beer.url));
-                                    
+
                                     found++;
                                 } else {
                                     msg.send(sprintf('(%s -> %s)', beer.name, beer.url));
                                 }
                             }
-                            
+
                             var num = object.num - found;
-                            msg.send(sprintf('%d %sresults...', num, num > 0 ? 'more ' : ''));
-                            
-                            break;
-                            
+                            return msg.send(sprintf('%d %sresults...', num, num > 0 ? 'more ' : ''));
+
                         case 'SCORE':
                             for (var i = 0, len = object.beer.length; i < len; i++) {
                                 var beer = object.beer[i];
-                                
+
                                 msg.send(sprintf('rating for "%s" = %s (%s)', beer.name, beer.score, beer.url));
                             }
-                            break;
+
+                            return;
                     }
-                    msg.send(object.alt);
-                    msg.send(object.title);
-                    return msg.send(object.img);
+
                 }
             });
         });
