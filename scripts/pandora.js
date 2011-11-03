7b87d70d9a0b1f846dd580e8518261809f6ce3d5
Justin Rainbow <justin.rainbow@gmail.com>
2011-11-03 16:28:31 -0700

    Adding lots of commands to pandora

--- scripts/pandora.js
+++ scripts/pandora.js
@@ -1,13 +1,14 @@
 
 module.exports = function(robot) {
     var fs = require('fs'),
-        exec = require('child_process').exec;
+        exec = require('child_process').exec,
+        sprintf = require("sprintf").sprintf;
     
     function cmd(command, cb) {
         exec('echo '+command+' > ' + process.env.HUBOT_PANDORA_CTL, cb);
     }
     
-    robot.hear(/pandora play/, function (msg) {
+    robot.hear(/^pandora play/, function (msg) {
         cmd('p', function (err, stdout, stderr) {
             if (err) {
                 msg.send('Error communicating with pianobar!');
@@ -18,7 +19,7 @@ module.exports = function(robot) {
         })
     });
     
-    robot.hear(/^pandora next/, function (msg) {
+    robot.hear(/^pandora (next|skip)/, function (msg) {
         cmd('n', function (err, stdout, stderr) {
             if (err) {
                 msg.send('Error communicating with pianobar!');
@@ -28,4 +29,54 @@ module.exports = function(robot) {
             msg.send('Moving on...');
         });
     });
+    
+    robot.hear(/^pandora i hate this song/, function (msg) {
+        cmd('-', function (err, stdout, stderr) {
+            if (err) {
+                msg.send('Error communicating with pianobar!');
+                return;
+            }
+            
+            msg.send('This song has been banned!');
+        })
+    });
+    
+    robot.hear(/^i'?m tired of this song/, function (msg) {
+        cmd('t', function (err, stdout, stderr) {
+            if (err) {
+                msg.send('Error communicating with pianobar!');
+                return;
+            }
+            
+            msg.send('This song has been removed for 30 days');
+        })
+    });
+
+    robot.hear(/^pandora i love this song/, function (msg) {
+        cmd('+', function (err, stdout, stderr) {
+            if (err) {
+                msg.send('Error communicating with pianobar!');
+                return;
+            }
+            
+            msg.send('This song has got some lovin\'!');
+        })
+    });
+    
+    robot.hear(/^pandora info/, function (msg) {
+        fs.readFile('/tmp/nowplaying', function (err, data) {
+            if (err) {
+                msg.send('Unable to determine what song is currently being played...');
+                return;
+            }
+            
+            try {
+                var song = JSON.parse(data.toString());
+                
+                msg.send('Playing "%s" by "%s"', song.title, song.artist);
+            } catch (e) {
+                return msg.send('There was a problem parsing the song data... fail!');
+            }
+        })
+    })
 };
