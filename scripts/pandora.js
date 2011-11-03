a5b5bd8b917add5532b7f989fd4fcddfa1e812ef
Justin Rainbow <justin.rainbow@gmail.com>
2011-11-03 16:05:58 -0700

    Fixing pandora script - can't use fs.writeFile for fifos

--- scripts/pandora.js
+++ scripts/pandora.js
@@ -1,9 +1,15 @@
 
 module.exports = function(robot) {
-    var fs = require('fs');
+    var fs = require('fs'),
+        exec = require('child_process').exec;
     
-    robot.hear(/^pandora play\s*(.+?)/, function (msg) {
-        fs.writeFile(process.env.HUBOT_PANDORA_CTL, 'p', function () {
+    robot.hear(/pandora play/, function (msg) {
+        exec('echo p > ' + process.env.HUBOT_PANDORA_CTL, function (err, stdout, stderr) {
+            if (err) {
+                msg.send('Error communicating with pianobar!');
+                return;
+            }
+            
             msg.send('Now playing... or maybe paused...');
         })
     });
