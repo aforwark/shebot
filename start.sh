b5fe2c3f1a4c288a7a1cc03c43d0b680fb867c47
Justin Rainbow <justin.rainbow@gmail.com>
2011-11-03 12:34:32 -0700

    Removing shortcut

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-HUBOT_IRC_NICK=hubher \
-HUBOT_IRC_SERVER=irc.freenode.net \
-HUBOT_IRC_ROOMS="#sheknowsdev" \
-REDISTOGO_URL="redis://devslave.local:6379/" \
-bin/hubot -a irc -n Hubot
